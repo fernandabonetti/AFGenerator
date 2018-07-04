3d71a3001f1823db8ac51b9e5d1a257fab4f97fa
fernandabonetti <fernandasbonetti@gmail.com>
2018-07-04 00:47:26 -0300

    Trying to read the grammar

--- src/reading.py
+++ src/reading.py
@@ -16,13 +16,13 @@ symbols = {}
 lines = data.read().splitlines()
 initialState = 0
 state = 64
-
+states['S'] = {}
 for i in range(0, len(lines)):
     for j in range(0, len(lines[i])):
-        if lines[i][0] != '<':                      #checks if it's a token or grammar
+        if lines[i][0] != '<':                      #checks if it's not a grammar
+
             if not initialState:
-                states['S'] = {}
-                state+= 1
+                state += 1
                 initialState = 1
                 states['S'].setdefault(lines[i][j],[]).append(chr(state))
                 states[chr(state)] = {}
@@ -30,4 +30,19 @@ for i in range(0, len(lines)):
                 states[chr(state)].setdefault(lines[i][j],[]).append(chr(state+1))
                 states[chr(state+1)] = {}
                 state +=1
+        else:
+            rule = lines[i].replace(' ::= ', ' | ').split(' | ')
+            print(rule)
+            inputState = rule[0][1]
+            print(inputState)
+            for m in range(1, len(rule)):
+                if rule[m][0] != 'eps':
+                    states[inputState].setdefault(rule[m][0],[]).append(rule[m][2])
+                else:
+                    states[inputState].setdefault(None,[]).append('eps')
+
     initialState = 0
+
+#prints the ordered states
+for key, value in sorted(states.items()):
+    print("{} : {}".format(key, value))
