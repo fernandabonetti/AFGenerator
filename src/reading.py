1a3c1b128f179b43722d669e531b224ebb0ba0c2
fernandabonetti <fernandasbonetti@gmail.com>
2018-10-12 16:46:36 -0300

    changing module

--- src/reading.py
+++ src/reading.py
@@ -15,60 +15,8 @@ if __name__ == "__main__":
 states = {}
 lines = data.read().splitlines()
 lines = list(filter(lambda a: a != '', lines))
-initialState = 0
-state = 64
-states['S'] = {}
-
-def check(state):
-    if state == 82:
-        state+=2
-    else:
-        state+=1
-    return state
-
 # Creates the finite automata structure
-for i in range(0, len(lines)):                          	# reads until the end of input
-    if lines[i][0] != "<":                              	# Insert the language tokens
-        for j in range(0, len(lines[i])):
-            if not initialState:
-                state = check(state)
-                initialState = 1
-                states['S'].setdefault(lines[i][j],[]).append(chr(state))
-                states[chr(state)] = {}
-            else:
-                state = check(state)
-                if state == 84:
-                    states[chr(state-2)].setdefault(lines[i][j],[]).append(chr(state))
-                else:
-                    states[chr(state-1)].setdefault(lines[i][j],[]).append(chr(state))
-                states[chr(state)] = {}
-        initialState = 0
-    else:                                             		 #Inserting the grammars
-        rule = lines[i].replace(' ::= ', ' | ').split(' | ') #remove useless symbols from each grammar rule
-        if rule[0][1] == 'S':                                #checks if it is the initial state 's'
-            createState = 0
-            inputState = rule[0][1]
-            for m in range(1, len(rule)):
-                if rule[m][0] != 'ε':
-                    if rule[0][1] != rule [m][1]:
-                        createState = 1
-                    states[inputState].setdefault(rule[m][0],[]).append(chr(state+1))
-                else:
-                    states[inputState].setdefault('ε',[]).append('ε')
-            if createState == 1:
-                state = check(state)
-                states[chr(state)]={}
-        else:
-            inputState = chr(state)
-            for m in range(1, len(rule)):
-                if rule[m][0] != 'ε':
-                    if rule[0][1] != rule[m][2]:
-                        state = check(state)
-                        states[chr(state)]={}
-                    states[inputState].setdefault(rule[m][0],[]).append(chr(state))
-                else:
-                    states[inputState].setdefault('ε',[]).append('ε')
-
+lfa.createAF(states, lines)
 lfa.fillFinal(states)
 lfa.orderedStates(states)
 lfa.determinize(states)
