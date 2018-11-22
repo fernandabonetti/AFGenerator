a737d360d9e48385ae48f8b2d1b0bdfbad604737
fernandabonetti <fernandasbonetti@gmail.com>
2018-11-22 20:46:59 -0200

    Monitoring the Stack

--- src/Syntax.py
+++ src/Syntax.py
@@ -4,36 +4,36 @@ class Syntax:
     def __init__(self, TS):
         self.TS = TS
         self.fita = []
+        self.symbols = {}
+        self.LALRTable = {}
+        self.productions = {}
         self.getFita()
         self.parseLALR()
+        self.syntaxAnalizer()
 
     def getFita(self):
         for token in self.TS:
             self.fita.append(token.id)
 
-    def parseSymbols(self, msymbols, symbols):
+    def parseSymbols(self, msymbols):
         for symbol in msymbols:
             for i in symbol:
-                symbols[i.attrib['Index']] = (i.attrib['Name'], i.attrib['Type'])
+                self.symbols[i.attrib['Name']] = (i.attrib['Index'], i.attrib['Type'])
 
     #saves the production number as key and the rule name and rule size as value
-    def parseProd(self, mproductions, productions):
+    def parseProd(self, mproductions):
         for prod in mproductions:
             for i in prod:
-                productions[i.attrib['Index']] = (i.attrib['NonTerminalIndex'], i.attrib['SymbolCount'])
+                self.productions[i.attrib['Index']] = (i.attrib['NonTerminalIndex'], i.attrib['SymbolCount'])
 
-    def parseTable(self, mtable, LALRTable):
+    def parseTable(self, mtable):
         for state in mtable:
             for action in state:
-                LALRTable[action.attrib['Index']] = {}
+                self.LALRTable[action.attrib['Index']] = {}
                 for i in action:
-                    LALRTable[action.attrib['Index']][i.attrib['SymbolIndex']] = (i.attrib['Action'], i.attrib['Value'])
+                    self.LALRTable[action.attrib['Index']][i.attrib['SymbolIndex']] = (i.attrib['Action'], i.attrib['Value'])
 
     def parseLALR(self):
-        symbols = {}
-        LALRTable = {}
-        productions = {}
-
         tree = ET.parse('../testcases/LALRTable.xml')
         root = tree.getroot()
 
@@ -42,6 +42,23 @@ class Syntax:
         mproductions = root.iter('m_Production')
         mtable = root.iter('LALRTable')
 
-        symbols = self.parseSymbols(msymbols, symbols)
-        productions = self.parseProd(mproductions, productions)
-        LALRTable = self.parseTable(mtable, LALRTable)
+        self.parseSymbols(msymbols)
+        self.parseProd(mproductions)
+        self.parseTable(mtable)
+
+    def syntaxAnalizer(self):
+        stack = []
+        stack.append('0')
+        #print(self.LALRTable)
+        print(self.productions)
+        for symbol in self.fita:
+            print(stack)
+            index = self.symbols[symbol][0]
+            action, value = self.LALRTable[stack[-1]][index]
+            if action == '1':
+                stack.append(value)
+            elif action == '2':
+                prod, size = self.productions[value]
+                size = int(size) * 2
+                del stack[len(stack)-size:-1]
+                print(stack)
